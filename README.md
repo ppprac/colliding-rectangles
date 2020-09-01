56ecf6743443ce072ada740b926cda100f85186b
ppprac <70270759+ppprac@users.noreply.github.com>
2020-09-01 22:26:32 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# colliding-rectangles
-Algorithm for colliding rectangles
+# p5.play-boilerplate
+Boiler plate for p5.play
---
+++ sketch.js
@@ -0,0 +1,18 @@
+var fixedrect, movingrect;
+
+function setup() 
+{
+  createCanvas(1200,800);
+ fixedrect =  createSprite(600, 400, 80, 80);
+ movingrect = createSprite(200,200, 80, 30);
+ fixedrect.shapeColor = "green";
+ movingrect.shapeColor = "green";
+ fixedrect.debug = true;
+ movingrect.debug = true;
+}
+
+function draw() 
+{
+  background(0,0,255);  
+  drawSprites();
+}
